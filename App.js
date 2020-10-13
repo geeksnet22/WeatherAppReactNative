58f7a7c7397900e2f4908db43beef99f805153a5
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2020-10-13 05:04:05 -0600

    fetch weather data in App.js and build CurrentWeather component in a different file, delete DataFetcher.js

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
-import DataFethcher from "./utils/DataFetcher";
+import CurrentWeather from "./components/CurrentWeather";
 
 function App() {
   const [location, setLocation] = useState("");
+
+  // current weather data
+  const [city, setCity] = useState();
+  const [temperature, setTemperature] = useState();
+  const [summary, setSummary] = useState();
+
+  // hourly weather data
+
   _getUserLocation = async () => {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") {
@@ -17,10 +25,34 @@ function App() {
     }
   };
   _getUserLocation();
+
+  useEffect(() => {
+    fetch(
+      "https://api.darksky.net/forecast/7d7c4d51abd38384fd51a174d0771a5d/37.8267,-122.4233"
+    )
+      .then((response) => response.json())
+      .then((json) => _setWeatherInfo(json))
+      .catch((error) => console.error(error));
+  }, [])
+
+  _setWeatherInfo = async (json) => {
+    console.log(json)
+    try {
+      let result = await Location.reverseGeocodeAsync({
+        latitude: json.latitude,
+        longitude: json.longitude,
+      });
+      setCity(result[0].city);
+      setTemperature(json.currently.temperature);
+      setSummary(json.currently.summary);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <DataFethcher />
-      <StatusBar style="auto" />
+      <CurrentWeather city={city} temperature={temperature} summary={summary}/>
     </View>
   );
 }
--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-function CurrentWeather() {
+function CurrentWeather(props) {
     return (
-        <View>
-            
+        <View style={styles.overallContainer}>
+            <Text style={styles.text}>{props.city}</Text>
+            <Text style={styles.text}>{`${
+            (((props.temperature - 32) * 5) / 9) | 0
+            } C`}</Text>
+            <Text style={styles.text}>{props.summary}</Text>
         </View>
-    )
+        );
 }
+
+const styles = StyleSheet.create({
+    overallContainer: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: "#87CEEB",
+      width: "100%",
+      padding: 30,
+    },
+    text: {
+      fontSize: 20,
+      color: "white",
+    },
+  });
+
+export default CurrentWeather;
